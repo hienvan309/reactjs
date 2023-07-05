173639a46321e45d168140f98e51ec7cdc63322b
Hien Van <namdinh3009@gmail.com>
2023-07-05 21:09:17 +0700

    Update antdesign

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Page1 from './Page1'
+import Page1 from './Page1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
