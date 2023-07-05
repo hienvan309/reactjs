78177396e234e866a042ea8b6c1465f3575baf08
Hien Van <namdinh3009@gmail.com>
2023-07-05 09:30:11 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Van đẹp trai
+          CỐ lên chàng trai
         </p>
         <a
           className="App-link"
